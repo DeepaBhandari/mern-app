48f0bbbbffa44e5009b8bcb1df82b3b6d6559cec
Deepa Bhandari <me.dpabhandari@gmail.com>
2024-02-19 20:39:32 +0545

    server file fix

--- server/model/Blog.js
+++ server/model/Blog.js
@@ -1,16 +1,17 @@
-const mongoose = require('mongoose');
-const schema = mongoose.schema;
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
 
 const blogSchema = new Schema({
-    title:{
-        type: String
-    },
-    description:{
-        type: String
-    },
-    date:{
-        type: Date,
-        default: Date.now
-    }
-})
+  title: {
+    type: String,
+  },
+  description: {
+    type: String,
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
 module.exports = mongoose.model("Blog", blogSchema);
