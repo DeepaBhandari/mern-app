ca0f902410925cbec1cda08a9da076ef3ff8a83e
Deepa Bhandari <me.dpabhandari@gmail.com>
2024-02-19 20:24:53 +0545

    addition of routing and posting new blog via axios

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import Header from './components/header';
-import {Routes, Route,Link} from 'react-router-dom';
+import {Routes, Route} from 'react-router-dom';
 import Home from './pages/home';
 import AddNewBlog from './pages/add-blog';
 
--- client/src/pages/add-blog/index.jsx
+++ client/src/pages/add-blog/index.jsx
@@ -1,7 +1,61 @@
+import classes from './styles.module.css';
+import { GlobalContext } from '../../context';
+import {useContext} from "react";
+import axios from 'axios';
+import {useNavigate} from 'react-router-dom';
+
 export default function AddNewBlog(){
+    const { formData, setFormData, setIsEdit, isEdit } =
+    useContext(GlobalContext);
+    const navigate = useNavigate();
+    
+    async function handleSaveBlogToDatabase(){
+     const response = await axios.post('http://localhost:5000/api/blogs/add',{
+        title: formData.title,
+        description: formData.description
+     })
+     const result = await response.data;
+     
+     console.log(result);
+
+     if(result){
+        setFormData({
+            title: '',
+            description: ''
+        })
+        navigate('/')
+     }
+    }
     return(
-        <div>
+        <div className={classes.wrapper}>
         <h1>Add A Blog</h1>
+        <div>
+            <input
+            name='title'
+            placeholder='Enter Blog Title'
+            id='title'
+            value= {formData.title}
+            onChange={(e)=>setFormData({
+                ...formData,
+                title:e.target.value,
+            })}
+            />
+             <textarea
+          name="description"
+          placeholder="Enter Blog Description"
+          id="description"
+          value={formData.description}
+          onChange={(event) =>
+            setFormData({
+              ...formData,
+              description: event.target.value,
+            })
+          }
+        />
+        <button onClick= {handleSaveBlogToDatabase}>
+          {isEdit ? "Edit Blog" : "Add Blog"}
+        </button>
+        </div>
         </div>
     )
 }
